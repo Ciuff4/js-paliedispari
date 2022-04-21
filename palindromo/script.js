d0fab81df56909f4037c197a9bd4e6974943c2da
Ciuff4 <gasperini_cristina@libero.it>
2022-04-21 09:02:31 +0200

    found function to reverse a word, then compared the words

--- palindromo/script.js
+++ palindromo/script.js
@@ -3,14 +3,24 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 Bonus: L’inserimento avviene tramite un campo input*/
 
-let parola= prompt("inserisci una parola:")
-let parola2;
+const parola= prompt("inserisci una parola:")
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    
+const parola2= reverseString(parola);
+console.log(parola2);
+
+let message;
+
+if (parola===parola2) {
+    message= "la parola è palindroma"
+}else{
+    message="la parola non è palindroma";
 }
 
-function name(params) {
-    
+console.log(message);
+
+function reverseString(str) {
+    for (var reversedStr = "", i = str.length - 1; i >= 0; i--) {
+        reversedStr += str[i];  
+    }   
+    return reversedStr;
 }
